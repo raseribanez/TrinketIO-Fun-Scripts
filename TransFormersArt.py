3646c8483d135fcb87afe70616f20b29a2610694
Ben Woodfield <raser.13@hotmail.com>
2016-12-05 19:34:33 +0000

    Yeah...Transformers Art

--- TransFormersArt.py
+++ TransFormersArt.py
@@ -1,4 +1,10 @@
-﻿print"──────────▄▄▄▄▄▄▄▄▄──────────"
+# Ben Woodfield
+# ALL I did here was search Google Images for Text / ASCII art
+# NOTE This won't really work in IDLE, id won't decode it
+# TrinketIO's online code engine DOES display it properly
+
+print"======== TRANSFORMERS ======="
+print"──────────▄▄▄▄▄▄▄▄▄──────────"
 print"───────▄█████████████▄───────"
 print"▐███▌─█████████████████─▐███▌"
 print"─████▄─▀███▄─────▄███▀─▄████─"
