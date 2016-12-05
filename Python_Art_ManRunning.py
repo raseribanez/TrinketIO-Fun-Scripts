13c090e6f502dd77693c81a18bcdcf9e473cec7a
Ben Woodfield <raser.13@hotmail.com>
2016-12-05 19:51:34 +0000

    Create Python_Art_ManRunning.py

---
+++ Python_Art_ManRunning.py
@@ -0,0 +1,29 @@
+# Ben Woodfield
+# I found this on a website when I found myself in a dead link
+# This little ascii art style drawing showed up (not sure if it was the website, or browser)
+# I use Chrome and Firefox so not sure which one it was 
+# Anyway....Loved it, turned it into Python art! Just uses print statements!
+
+print"|========== PYTHON ART   ===========|"
+print" \                           /     "
+print"  \                         /      "
+print"   \                       /       "
+print"    ]                     [    ,'| "
+print"    ]                     [   /  | "
+print"    ]___               ___[ ,'   | " 
+print"    ]  ]\             /[  [ |:   | " 
+print"    ]  ] \           / [  [ |:   | " 
+print"    ]  ]  ]         [  [  [ |:   | "
+print"    ]  ]  ]__     __[  [  [ |:   | "
+print"    ]  ]  ] ]\ _ /[ [  [  [ |:   | "
+print"    ]  ]  ] ] (O) [ [  [  [ :====' "
+print"    ]  ]  ]_].nHn.[_[  [  [        "
+print"    ]  ]  ]  HHHHH. [  [  [        "
+print"    ]  ] /   `HH('N  \ [  [        "  
+print"    ]__]/     HHH  U   \[_[        "
+print"    ]         NNN         [        "
+print"    ]         N/U         [        "
+print"    ]         N H         [        "
+print"   /          N            \       "
+print"  /           q,            \      "
+print" /                           \     "
